d1174fce2ca1f883f73db378b9bfebe9c30328c8
Agnesh S Kumar <agnesh.skumar@gadgeon.com>
2022-09-07 12:50:20 +0530

    Displaying user detail on side drawer

--- src/components/CustomNavigationDrawer.js
+++ src/components/CustomNavigationDrawer.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, Text, ImageBackground, Image, TouchableOpacity } from 'react-native';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
-
+import { app } from '../../api/FirebaseConfig';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const CustomDrawer = props => {
+
+    const auth = getAuth(app)
+    const userEmail = auth.currentUser?.email
+    const [username, setUsername] = useState('')
+    const [imageUrl, setImageUrl] = useState('')
+
+    const getProfileData = async function(){
+
+        const firestore = getFirestore(app)
+        const docRef = doc(firestore, "USERS", userEmail)
+        const docSnap = await getDoc(docRef)
+
+        if(docSnap.exists())
+        {
+            const data = docSnap.data()
+            setUsername(data.username)
+            setImageUrl(data.image_uri)
+        }
+        else
+        {
+            //Toaster("Some error occurred")
+        }
+    }
+
+    useEffect( ()=> {getProfileData()},[])
+
     return (
         <View style={{ marginTop:-40, flex: 1 }}>
             
             <DrawerContentScrollView {...props} contentContainerStyle={{ backgroundColor: '#8200d6' }}>
                 <ImageBackground source={require('../../assets/headerBG.jpg')} style={{ paddingTop: 50, paddingLeft: 15}}>
-                    <Image source={require('../../assets/user.png')} style={{ height: 80, width: 80, borderRadius: 40, marginBottom: 10 }} />
-                    <Text style={{ color: '#fff', fontSize: 18, marginBottom: 5 }}> John Doe </Text>
-                    <Text style={{ color: '#fff', fontSize: 18, marginBottom: 10 }}> john@gmail.com </Text>
+                    {imageUrl && <Image source={{uri: imageUrl}} style={{ height: 80, width: 80, borderRadius: 40, marginBottom: 10 }} />}
+                    {imageUrl === "" && <Image source={require("../../assets/user.png")} style={{ height: 80, width: 80, borderRadius: 40, marginBottom: 10 }} />}
+                    <Text style={{ color: '#fff', fontSize: 18, marginBottom: 5 }}> {username} </Text>
+                    <Text style={{ color: '#fff', fontSize: 18, marginBottom: 10 }}> {userEmail} </Text>
                 </ImageBackground>
 
                 <View style={{ flex: 1, backgroundColor: '#fff', paddingTop: 10 }}>
